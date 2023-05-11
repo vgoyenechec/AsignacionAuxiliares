39c85886c576b628e6a641bec39eceb3eae898a6
Valentina <valegoye@gmail.com>
2023-05-11 11:53:48 -0500

    Initial commit

---
+++ src/main/java/ReggioAmelia/AsignacionAuxiliares/AsignacionAuxiliaresApplication.java
@@ -0,0 +1,13 @@
+package ReggioAmelia.AsignacionAuxiliares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignacionAuxiliaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignacionAuxiliaresApplication.class, args);
+	}
+
+}
